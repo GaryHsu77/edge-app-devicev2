9381bc8b409142fe5c1e94a96a1bfaec29764cd3
GaryHsu77 <bibbylong@gmail.com>
2019-03-29 23:52:01 +0800

    chore: amd64 build complete

--- root/usr/local/sbin/moxa.sh
+++ root/usr/local/sbin/moxa.sh
@@ -19,8 +19,10 @@ for d in /usr/lib/sanji-1.0/* ; do
     fi
 done
 
-systemctl enable thingspro-gateway-web-service
 systemctl start thingspro-gateway-web-service
+systemctl start watchdog.service
+systemctl start monit.service
+systemctl start mosquitto.service
 
 export BUNDLES_HOME=/usr/lib/sanji-1.0
 export BUNDLE_ENV=production
