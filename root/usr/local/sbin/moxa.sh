dad0fe807ab9e73b07d23d981eea468335ee88ce
GaryHsu77 <bibbylong@gmail.com>
2019-03-29 10:41:57 +0800

    chore: change config path outside apps

--- root/usr/local/sbin/moxa.sh
+++ root/usr/local/sbin/moxa.sh
@@ -19,7 +19,10 @@ for d in /usr/lib/sanji-1.0/* ; do
     fi
 done
 
+systemctl start thingspro-gateway-web-service
+
 export BUNDLES_HOME=/usr/lib/sanji-1.0
 export BUNDLE_ENV=production
+export MX_DATA_LOG_PATH=/var/mxc
 
 exec python /usr/lib/sanji-1.0/bootstrap/bootstrap.py
